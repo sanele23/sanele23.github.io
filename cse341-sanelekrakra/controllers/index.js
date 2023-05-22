6e2a3b58e34841714ef000bc9f3ffce8ce33bee5
sanele23 <sanele23@live.com>
2023-05-22 16:03:55 +0200

    Updated file

--- cse341-sanelekrakra/controllers/index.js
+++
@@ -1,7 +0,0 @@
-// function - get response when sending request from home page
-
-const awesomeFunction = ('/', (req, res, next) => {
-    res.json('Thobeka Dvuba');
-});
-
-module.exports = {awesomeFunction};
--- cse341-sanelekrakra/routes/index.js
+++
@@ -1,7 +0,0 @@
-const routes = require('express').Router();
-
-const myController = require('../controllers');
-
-routes.get('/', myController.awesomeFunction);
-
-module.exports = routes;
